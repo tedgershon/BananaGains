eab056fc13aa7c8e05c75f72ce05a60486d1d60a
thesoupmanguy@gmail.com <thesoupmanguy@gmail.com>
2026-04-12 17:28:07 -0400

    redirect home page to markets page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,1 @@
-"use client";
-
-import { HottestMarketDisplay } from "@/components/hottest-market";
-import { TrendingTopMarkets } from "@/components/trending-markets";
-import { WeeklyLeaderboard } from "@/components/weekly-leaderboard";
-
-export default function Home() {
-  return (
-    <div className="mt-2 space-y-8">
-      <HottestMarketDisplay />
-
-      <div className="grid gap-8 lg:grid-cols-2">
-        <WeeklyLeaderboard />
-        <TrendingTopMarkets />
-      </div>
-    </div>
-  );
-}
+export { default } from "./markets/page";
